12005352943de68551761c8e32c6246c624796c8
OmDoulagar <omdoulagar12345@gmail.com>
2021-09-07 18:32:18 +0530

    Revert "violet: "Inbuilt ANX Camera\""
    This reverts commit c6bd434a5a677d109c65f2378560ee14f8964824.

--- bliss_violet.mk
+++ bliss_violet.mk
@@ -19,9 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit ANX Camera
-$(call inherit-product, vendor/ANXCamera/config.mk)
-
 # Inherit some common WaveOS stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 $(call inherit-product, vendor/bliss/configs/common.mk)
